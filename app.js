3abb303bc2eb2132c9de71509ec05e06a7e2c7b7
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-02-12 22:19:28 +0200

    example

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 
+const SwaggerParser = require('@apidevtools/swagger-parser');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,6 +14,13 @@ app.use(cors());
 
 const serverPort = process.env.SERVER_PORT;
 
-app.listen(serverPort, () =>
-  console.log(`SERVER IS RUNNING ON PORT ${serverPort}`)
-);
+app.listen(serverPort, () => {
+  console.log(`SERVER IS RUNNING ON PORT ${serverPort}`);
+  SwaggerParser.dereference('data-sets/pet-store-api.json', (err, api) => {
+      if (err) {
+        return console.log('ERROR');
+      }
+
+      console.log(api.paths['/pets'].post.parameters[0].schema);
+  })
+});
