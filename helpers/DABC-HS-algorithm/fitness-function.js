61f1072a1fb9be549c994f579391e0f9b391ea33
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-03-12 22:36:12 +0200

    fitness

---
+++ helpers/DABC-HS-algorithm/fitness-function.js
@@ -0,0 +1,29 @@
+exports.fitness = (population, routeObj, numbers) => {
+  // check Operations Coverage
+  const numberOfOperations = new Set(population.map((ch) => ch.operation)).size;
+  const operationCoverage =
+    numberOfOperations / numbers.totalNumberOfOperations;
+
+  // check Paramters Coverage
+  const numberOfInputs =
+    Math.max(...population.map((ch) => ch.parameters.length)) +
+    Math.max(...population.map((ch) => ch.bodyContent.length));
+  const parameterCoverage = numberOfInputs / numbers.totalNumberOfInputs;
+
+  // check Parameter value coverage
+  let parameterValueCoverage = 0;
+  if (numbers.totalNumberOfFinitValues != 0) {
+    const finitParams = [];
+    population.forEach((ch) => {
+      finitParams.push(...ch.parameters.filter((param) => param.isFinite));
+    });
+    const numberOfFiniteValues = new Set(
+      finitParams.map((param) => param.value)
+    ).size;
+
+    parameterValueCoverage =
+      numberOfFiniteValues / numbers.totalNumberOfFinitValues;
+  }
+
+  // TODO: check Status code coverage
+};
