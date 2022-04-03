f96891f356d9c2bcba5be2521fe5abb45ca39c22
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-04-03 02:36:34 +0200

    #

--- helpers/DABC-HS-algorithm/fitness-function.js
+++ helpers/DABC-HS-algorithm/fitness-function.js
@@ -7,11 +7,15 @@ exports.fitness = (chromosome, numbers) => {
 
   // check Paramters Coverage
   // by: dividing number of overall inputs used in testcase / total expected number of inputs
-  const numberOfInputs =
-    Math.max(...chromosome.map((ch) => ch.parameters.length)) +
-    Math.max(...chromosome.map((ch) => ch.properties.length));
+  const numberOfInputs = new Set(
+    chromosome.reduce((filteredCh, ch) => {
+      filteredCh.push(...ch.parameters.map((param) => param.name));
+      filteredCh.push(...ch.properties.map((prop) => prop.name));
+      return filteredCh;
+    }, [])
+  ).size;
   const parameterCoverage = numberOfInputs / numbers.totalNumberOfInputs;
-  
+
   // check Inputs value Coverage
   // by: dividing number of distinct finite values(boolean|enums) used in testcase / total expected finite values
   let inputValueCoverage = 0;
@@ -39,7 +43,7 @@ exports.fitness = (chromosome, numbers) => {
   const totalNumberOfExpectedStatusCode = numberOfOperations * 2;
   const numberOfExpectedStatusCode = new Set(
     chromosome.map((ch) => ch.operation + ch.testType)
-  ).size; 
+  ).size;
 
   const statusCodeCoverage =
     numberOfExpectedStatusCode / totalNumberOfExpectedStatusCode;
@@ -51,13 +55,14 @@ exports.fitness = (chromosome, numbers) => {
   // by: dividing number of overall mutated inputs used in testcase / total expected number of mutated inputs
   // only in mutation test cases type
   // STILL IN TESTING........................................
-  // const numberOfMutatedSet = new Set();
-  // chromosome.forEach(ch => {
-  //   if (ch.testType === 'mutation') {
-  //     numberOfMutatedSet.add(...ch.mutationApplied.map((ma) => ma.inputName));
-  //   }
-  // });
-  // const numberOfMutatedInputs = numberOfMutatedSet.size;
+  // const numberOfMutatedInputs = new Set(
+  //   chromosome.reduce((filteredCh, ch) => {
+  //     ch.testType === 'mutation'
+  //       ? filteredCh.push(...ch.mutationApplied.map((ma) => ma.inputName))
+  //       : null;
+  //     return filteredCh;
+  //   }, [])
+  // ).size;
   // const mutatedParameterCoverage =
   //   numberOfMutatedInputs / numbers.totalNumberOfInputs;
 
