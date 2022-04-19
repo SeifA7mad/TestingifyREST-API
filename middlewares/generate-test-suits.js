1b3bb3006a833ae48413747d2a1040123386272e
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-04-19 04:49:46 +0200

    ONLOOKERBEE-MODIFIED

--- middlewares/generate-test-suits.js
+++ middlewares/generate-test-suits.js
@@ -9,6 +9,16 @@ const {
   mutateStructure,
 } = require('../helpers/DABC-HS-algorithm/mutation-funtion');
 
+const calculateProbabilities = (fitnessValues, maxFit) => {
+  const probabilities = [];
+  fitnessValues.forEach(value => {
+    const propValue = 0.9 * (value / maxFit) + 0.1;
+    probabilities.push(propValue);
+  });
+
+  return probabilities;
+}
+
 exports.generateTestSuits = (req, res, next) => {
   //! ............................................Initialization Phase..........................................................
   const populationSize = Object.keys(req.routes).length;
@@ -16,6 +26,8 @@ exports.generateTestSuits = (req, res, next) => {
   const fitnessValues = new Array(populationSize);
   // maximum number of fitness evaluations
   const mfe = 100;
+  // maximum value of fitness function
+  const mfv = populationSize * 3;
   // The maximum number of the trials to determine exhausted sources
   const limit = 3;
   // first population => currentPopulation
@@ -50,27 +62,34 @@ exports.generateTestSuits = (req, res, next) => {
     //! ....................................................END..................................................................
 
     //! ............................................Onlooker Bee phase...........................................................
-    for (let i = 0; i < populationSize; i++) {
-      if (Math.random() > 1 / populationSize) {
-        continue;
+    const prob = calculateProbabilities(fitnessValues, mfv);
+    let j = 0
+    let k = 0;
+    while (k < populationSize) {
+      if (Math.random() < prob[j]) {
+        const testCase = currentPopulation[populationKeys[j]]['testCase'];
+        const numbers = currentPopulation[populationKeys[j]]['numbers'];
+
+        const newTestCase = mutateStructure(
+          testCase,
+          req.routes[routesKeys[j]]
+        );
+
+        const oldFitnessValue = fitness(testCase, numbers);
+
+        const newFitnessValue = fitness(newTestCase, numbers);
+
+        if (newFitnessValue > oldFitnessValue) {
+          currentPopulation[populationKeys[j]]['testCase'] = newTestCase;
+          trials[j] = 0;
+          fitnessValues[j] = newFitnessValue;
+        } else {
+          trials[j]++;
+          fitnessValues[j] = oldFitnessValue;
+        }
+        k++;
       }
-      const testCase = currentPopulation[populationKeys[i]]['testCase'];
-      const numbers = currentPopulation[populationKeys[i]]['numbers'];
-
-      const newTestCase = mutateStructure(testCase, req.routes[routesKeys[i]]);
-
-      const oldFitnessValue = fitness(testCase, numbers);
-
-      const newFitnessValue = fitness(newTestCase, numbers);
-
-      if (newFitnessValue > oldFitnessValue) {
-        currentPopulation[populationKeys[i]]['testCase'] = newTestCase;
-        trials[i] = 0;
-        fitnessValues[i] = newFitnessValue;
-      } else {
-        trials[i]++;
-        fitnessValues[i] = oldFitnessValue;
-      }
+      j = ++j % populationSize;
     }
     //! ....................................................END..................................................................
 
