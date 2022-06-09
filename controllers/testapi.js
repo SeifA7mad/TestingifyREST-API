2d6801d3892ec8a5605e2cc9ac6d45dd40ae88bf
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-06-09 10:48:25 +0200

    newRoute

--- controllers/testapi.js
+++ controllers/testapi.js
@@ -1,7 +1,7 @@
 exports.postTestApi = (req, res, next) => {
   console.log('TEST DATA SENT!!');
   res.status(200).json({
-    testResults: req.testSuiteResults ? req.testSuiteResult : null,
+    testResults: req.testSuiteResults ? req.testSuiteResults : null,
     routes: Object.keys(req.testSuite),
     fitnessValues: req.fitnessValues,
     sumFitnessValues: req.sumFitnessValues,
